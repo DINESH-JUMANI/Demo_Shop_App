49295c6f4e8377e091f05c18700c7c7d0579f25e
DINESH-JUMANI <jumanidinesh981@gmail.com>
2026-02-11 01:15:50 +0530

    ADD: Added the assets

---
+++ lib/core/constants/app_assets.dart
@@ -0,0 +1,11 @@
+class AppAssets {
+  // Base paths
+  static const String _imagesPath = 'assets/images';
+  static const String _iconsPath = 'assets/icons';
+
+  // Images
+  static const String noDataImage = '$_imagesPath/no_data.png';
+
+  // Icons
+  static const String appIcon = '$_iconsPath/app_icon.png';
+}
